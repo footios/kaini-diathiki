29c84e1f44acf652d2593960b795636909203022
Fotis Tsakiris <footios76@gmail.com>
2019-04-04 18:54:24 +0300

    Modal and Books animation OK, but div of Modal...

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -15,23 +15,20 @@ class Books extends Component {
 		books: [ 'ΚΑΤΑ ΜΑΤΘΑΙΟΝ', 'ΚΑΤΑ ΜΑΡΚΟΝ', 'ΚΑΤΑ ΛΟΥΚΑΝ', 'ΚΑΤΑ ΙΩΑΝΝΗΝ' ]
 	};
 	render() {
+		// let books = [<div className={classes.DivBeforeShowBooks}></div>];
 		let books = null;
 		if (this.props.onEntered) {
-			 books = (
-				<ul style={{ listStyleType: 'none' }}>
-					{this.state.books.map((book, index) => (
-						<CSSTrantition key={index} timeout={300} classNames="fade">
-							<li className={classes.ListItem} style={{ textAlign: 'left' }}>
-								<Book name={book} />
-							</li>
-						</CSSTrantition>
-					))}
-				</ul>
-			);
+			books = this.state.books.map((book, index) => (
+				<CSSTrantition key={index} timeout={1000} classNames={{ enterActive: classes.ShowBooks }}>
+					<li className={classes.ListItem} style={{ textAlign: 'left' }}>
+						<Book name={book} />
+					</li>
+				</CSSTrantition>
+			));
 		}
-		
+
 		return (
-			<TranstitionGroup component="ul" className={classes.List} timeout={300}>
+			<TranstitionGroup component="ul" className={classes.List} timeout={1000}>
 				{books}
 			</TranstitionGroup>
 		);
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import Books from '../../Books/Books';
 
-
 import classes from './Modal.module.css';
 
 const modal = (props) => {
 	const cssClasses = [
 		classes.Modal,
-		props.show === 'entering' ? classes.ModalOpen : props.show === 'exiting' ? classes.ModalClosed : null
+		props.show === 'entered' ? classes.ModalOpen : props.show === 'exiting' ? classes.ModalClosed : null
 	];
 
 	return (
-		<div className={cssClasses.join(' ')}>
-				<Books onEntered={props.onEntered} />
-				<button onClick={props.clicked} className={classes.Button}>
-					Έξοδος
-				</button>
-			
+		<div className={cssClasses.join(' ')} onClick={props.clicked}>
+			<Books onEntered={props.onEntered} />
 		</div>
 	);
 };
